6aba59e511f60d5eced15f7a0c79279d808f4bb1
bijay <bijaykhadka765@gmail.com>
2023-05-03 19:34:24 +0545

    add all the file

---
+++ circumferenece.js
@@ -0,0 +1,4 @@
+const radius = prompt("Enter the radius of the circle:");
+const circumference = 2 * Math.PI * radius;
+console.log(`The circumference of a circle with radius ${radius} is ${circumference}`);
+ 
